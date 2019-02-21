2bb40f93ebdfe649705b08f72f4cbdb45b9ba120
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-21 23:42:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Dog 
+ 
+  @@all = []
+  @@all_names = []
+  
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self 
+    @@all_names << name 
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def self.all 
+    @@all_names.each do |name|
+      puts name 
+    end
+  end 
+  
+  
+end 
+
